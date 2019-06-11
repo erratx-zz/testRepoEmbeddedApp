622aa3f8ee1ec909eabce031d32c95829f0b3567
Ethan Roberts <etroberts@tableau.com>
2019-06-10 20:06:07 -0700

    Updated mainApp; removed extraneous code

--- app.js
+++ app.js
@@ -13,7 +13,6 @@ app.set('views',__dirname + '/views');
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: false}));
 app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, 'public')));
 app.use(fileUpload());
 
 app.get('/', getHomePage);
