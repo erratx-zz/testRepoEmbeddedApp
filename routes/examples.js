f92a93533ddaf4092058f062a7ba61c5170b7a82
Ethan Roberts <etroberts@tableau.com>
2019-06-12 14:26:47 -0700

    altered the navigation to use ejs

--- routes/examples.js
+++ routes/examples.js
@@ -2,13 +2,13 @@ module.exports = {
     getExamplesHomePage: (req, res) => {
         res.render('examples_home.ejs', {
             title: "JavaScript API | Examples Homepage",
-            example_num = 0;
+            example_num = 0
         });
     },
     getBasicEmbedPage: (req, res) => {
         res.render('basic_embed.ejs', {
             title: "JavaScript API | Basic Embed Examples",
-            example_num: 1;
+            example_num: 1
         });
     },
 };
