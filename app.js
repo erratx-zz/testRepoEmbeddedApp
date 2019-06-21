3c2932f095387133956df784c33fea1f7001e785
Ethan Roberts <etroberts@tableau.com>
2019-06-21 08:28:12 -0500

    Added cat picture

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ app.set('views',__dirname + '/views');
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: false}));
 app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(fileUpload());
 
 app.get('/', getHomePage);
